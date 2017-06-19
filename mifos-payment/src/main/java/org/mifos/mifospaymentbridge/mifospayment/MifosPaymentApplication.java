10d5d0c350c577a6f4e6f6ac35bfca95d0ca57e9
Vladimir Fomene <vladimirfomene@gmail.com>
2017-06-19 12:35:33 +0100

    Initialization of mifos-payment-gateway

---
+++ mifos-payment/src/main/java/org/mifos/mifospaymentbridge/mifospayment/MifosPaymentApplication.java
@@ -0,0 +1,12 @@
+package org.mifos.mifospaymentbridge.mifospayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MifosPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MifosPaymentApplication.class, args);
+	}
+}
