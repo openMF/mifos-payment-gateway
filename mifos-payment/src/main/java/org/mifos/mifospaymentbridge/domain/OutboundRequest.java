654c80b5d72a126bf41caa9b323f2bbd9073f0d0
Vladimir Fomene <vladimirfomene@gmail.com>
2017-06-28 18:41:48 +0100

    Create file for outbound request from fineract

---
+++ mifos-payment/src/main/java/org/mifos/mifospaymentbridge/domain/OutboundRequest.java
@@ -0,0 +1,20 @@
+/**
+ * File: OutboundRequest.java
+ * =========================================
+ * This class models the mifos payment gateway outbound-request
+ * table in our database. It serves as an object to which data from the
+ * fineract api is bind on every outbound transaction
+ * @author Vladimir Fomene
+ **/
+
+package org.mifos.mifospaymentbridge.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+public class OutboundRequest{
+
+}
