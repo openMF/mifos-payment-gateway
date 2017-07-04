fd26b7ec4793fd8489d8c10ae31f608d4fbf8dda
Vladimir Fomene <vladimirfomene@gmail.com>
2017-07-04 19:25:53 +0100

    Create a model for the category entity

---
+++ mifos-payment/src/main/java/org/mifos/mifospaymentbridge/model/Category.java
@@ -0,0 +1,103 @@
+/**
+ * File: Category.java
+ * =========================================
+ * This class models the category
+ * table in our database.
+ * @author Vladimir Fomene
+ **/
+
+package org.mifos.mifospaymentbridge.model;
+
+import org.joda.time.DateTime;
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    /**
+     * table Fields of the category table
+     */
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name="category_id")
+    private Long id;
+
+    @Column(name="category_name")
+    private String categoryName;
+
+    @Column(name="last_modified_dtm")
+    private DateTime lastModifiedDtm;
+
+    @Column(name="last_modified_by_id")
+    private Integer lastModifiedById;
+
+    /**
+     * Gets the id of a category
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Sets the id of a category
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets the name of a category
+     * @return categoryName
+     */
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    /**
+     * Sets the category name of a category
+     * @param categoryName
+     */
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    /**
+     * Get the last modified date
+     * @return lastModifiedDtm
+     */
+    public DateTime getLastModifiedDtm() {
+        return lastModifiedDtm;
+    }
+
+    /**
+     * Sets the last modified date
+     * @param lastModifiedDtm
+     */
+    public void setLastModifiedDtm(DateTime lastModifiedDtm) {
+        this.lastModifiedDtm = lastModifiedDtm;
+    }
+
+    /**
+     * Get the id which last modified the category
+     * @return lastModifiedById
+     */
+    public Integer getLastModifiedById() {
+        return lastModifiedById;
+    }
+
+    /**
+     * Sets the id that last modified the category
+     * @param lastModifiedById
+     */
+    public void setLastModifiedById(Integer lastModifiedById) {
+        this.lastModifiedById = lastModifiedById;
+    }
+}
