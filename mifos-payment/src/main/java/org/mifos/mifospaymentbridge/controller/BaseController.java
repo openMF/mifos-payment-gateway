bdd88f507b545f3ce15d08aa8cf20b12e28b70e9
Vladimir Fomene <vladimirfomene@gmail.com>
2017-07-13 19:55:54 +0100

    Add loan disbursement controller

---
+++ mifos-payment/src/main/java/org/mifos/mifospaymentbridge/controller/BaseController.java
@@ -0,0 +1,15 @@
+/**
+ *  File: BaseController.java
+ *  ====================================
+ *  This class implements all the general functionalities
+ *  needed by all controllers
+ *  @author Vladimir Fomene
+ */
+
+package org.mifos.mifospaymentbridge.controller;
+
+
+public abstract class BaseController {
+
+    public static final String FINERACT_API_PATH = "/api/v1";
+}
--- mifos-payment/src/main/java/org/mifos/mifospaymentbridge/controller/LoanDisbursementController.java
+++ mifos-payment/src/main/java/org/mifos/mifospaymentbridge/controller/LoanDisbursementController.java
@@ -3,5 +3,5 @@ package org.mifos.mifospaymentbridge.controller;
 /**
  * Created by vladimirfomene on 7/5/17.
  */
-public class LoanDisbursementController {
+public class LoanDisbursementController extends BaseController{
 }
