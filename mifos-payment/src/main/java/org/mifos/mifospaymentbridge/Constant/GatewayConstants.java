652743f8875880df3d01eb7f42c61f1e2073a833
Vladimir Fomene <vladimirfomene@gmail.com>
2017-07-21 13:31:20 +0100

    Create GatewayConstants.java for gateway constants

---
+++ mifos-payment/src/main/java/org/mifos/mifospaymentbridge/Constant/GatewayConstants.java
@@ -0,0 +1,18 @@
+/**
+ * File: GatewayConstants.java
+ * ===============================================
+ * A declaration of all the constants used in the payment
+ * gateway.
+ * @author Vladimir Fomene
+ */
+
+package org.mifos.mifospaymentbridge.Constant;
+
+
+public class GatewayConstants {
+    public static final int PAYMENT_API_STATUS = 1;
+
+    public static final int SYSTEM_STATUS = 2;
+
+    public static final int PAYMENT_STATUS = 3;
+}
